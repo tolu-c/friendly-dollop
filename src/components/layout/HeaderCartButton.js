c4c5812cde01c24a8b535b9b8c70228bf33b42bb
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-04-30 08:50:27 +0100

    used the context

--- src/components/layout/HeaderCartButton.js
+++ src/components/layout/HeaderCartButton.js
@@ -1,13 +1,25 @@
+import { useContext } from "react";
 import CartIcon from "../cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 function HeaderCartButton(props) {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItem = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount
+  }, 0)
+
   return (
-    <button className='flex items-center space-x-3 cursor-pointer' onClick={props.onClick}>
+    <button
+      className="flex items-center space-x-3 cursor-pointer"
+      onClick={props.onClick}
+    >
       <div className="relative">
         <CartIcon />
-        <span className='absolute -top-0.5 -right-1 h-4 w-4 rounded-full bg-orange-200 text-orange-700 flex items-center justify-center font-bold text-sm'>3</span>
+        <span className="absolute -top-0.5 -right-1 h-4 w-4 rounded-full bg-orange-200 text-orange-700 flex items-center justify-center font-bold text-sm">
+          {numberOfCartItem}
+        </span>
       </div>
-      <span className='text-sm'>Your Cart</span>
+      <span className="text-sm">Your Cart</span>
     </button>
   );
 }
