c339144692a2e06a97f92291c0da3edfc2671a00
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-05-21 13:26:15 +0100

    form validitity

--- src/components/cart/Checkout.js
+++ src/components/cart/Checkout.js
@@ -1,18 +1,53 @@
-import {useRef} from "react";
+import { useRef, useState } from "react";
+
+const isEmpty = (value) => value.trim() === "";
+const isFiveChars = (value) => value.trim().length === 5;
 
 function Checkout(props) {
-  const nameRef = useRef()
+  const [formValidity, setFormValidity] = useState({
+    name: true,
+    street: true,
+    postalCode: true,
+    city: true,
+  });
+
+  const nameRef = useRef();
   const streetRef = useRef();
   const postalCodeRef = useRef();
   const cityRef = useRef();
 
+  // form submit function
   const confirmHandler = (event) => {
     event.preventDefault();
 
-    const enteredName = nameRef.current.value
+    const enteredName = nameRef.current.value;
     const enteredStreet = streetRef.current.value;
     const enteredPostalCode = postalCodeRef.current.value;
     const enteredCity = cityRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+    const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
+
+    setFormValidity({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      postalCode: enteredPostalCodeIsValid,
+      city: enteredCityIsValid,
+    });
+
+    const formIsValid =
+      enteredCityIsValid &&
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredPostalCodeIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+
+    // submit form values
   };
 
   return (
@@ -20,10 +55,10 @@ function Checkout(props) {
       onSubmit={confirmHandler}
       className="w-full border-amber-200 border-t flex flex-col items-center space-y-3 overflow-y-scroll h-52"
     >
-      <div className="w-5/6 flex space-x-2 items-center mt-2">
+      <div className="w-5/6 flex space-x-2 items-center flex-start mt-2">
         <label
           htmlFor="name"
-          className="capitalize text-lg font-medium text-slate-600"
+          className="capitalize text-lg font-medium text-slate-600 basis-2/6"
         >
           Name
         </label>
@@ -31,14 +66,15 @@ function Checkout(props) {
           type="text"
           id="name"
           ref={nameRef}
-          className="grow focus:border-amber-400 focus:ring-1 focus:ring-amber-400 text-base font-sans text-slate-700"
+          className="basis-4/6 focus:border-amber-400 focus:ring-1 focus:ring-amber-400 text-base font-sans text-slate-700"
         />
+        {!formValidity.name && <p className="basis-6/6">enter a valid name!</p>}
       </div>
 
-      <div className="w-5/6 flex space-x-2 items-center mt-2">
+      <div className="w-5/6 flex space-x-2 items-center flex-start mt-2">
         <label
           htmlFor="street"
-          className="capitalize text-lg font-medium text-slate-600"
+          className="capitalize text-lg font-medium text-slate-600 basis-2/6"
         >
           street
         </label>
@@ -46,14 +82,17 @@ function Checkout(props) {
           type="text"
           id="street"
           ref={streetRef}
-          className="grow focus:border-amber-400 focus:ring-1 focus:ring-amber-400 text-base font-sans text-slate-700"
+          className="grow focus:border-amber-400 focus:ring-1 focus:ring-amber-400 text-base font-sans text-slate-700 basis-4/6"
         />
+        {!formValidity.street && (
+          <p className="basis-6/6">enter a valid street!</p>
+        )}
       </div>
 
-      <div className="w-5/6 flex space-x-2 items-center mt-2">
+      <div className="w-5/6 flex space-x-2 items-center flex-start mt-2">
         <label
           htmlFor="postalCode"
-          className="capitalize text-lg font-medium text-slate-600"
+          className="capitalize text-lg font-medium text-slate-600 basis-2/6"
         >
           postal Code
         </label>
@@ -61,14 +100,17 @@ function Checkout(props) {
           type="text"
           id="postalCode"
           ref={postalCodeRef}
-          className="grow focus:border-amber-400 focus:ring-1 focus:ring-amber-400 text-base font-sans text-slate-700"
+          className="grow focus:border-amber-400 focus:ring-1 focus:ring-amber-400 text-base font-sans text-slate-700 basis-4/6"
         />
+        {!formValidity.postalCode && (
+          <p className="basis-6/6">enter a valid postal code!</p>
+        )}
       </div>
 
-      <div className="w-5/6 flex space-x-2 items-center mt-2">
+      <div className="w-5/6 flex space-x-2 items-center flex-start mt-2">
         <label
           htmlFor="city"
-          className="capitalize text-lg font-medium text-slate-600"
+          className="capitalize text-lg font-medium text-slate-600 basis-2/6"
         >
           city
         </label>
@@ -76,8 +118,9 @@ function Checkout(props) {
           type="text"
           ref={cityRef}
           id="city"
-          className="grow focus:border-amber-400 focus:ring-1 focus:ring-amber-400 text-base font-sans text-slate-700"
+          className="grow focus:border-amber-400 focus:ring-1 focus:ring-amber-400 text-base font-sans text-slate-700 basis-4/6"
         />
+        {!formValidity.city && <p className="basis-6/6">enter a valid city!</p>}
       </div>
 
       <div className="w-full grid grid-cols-2 gap-x-4">
